9fbc1fadc0f1c253f46a57e5c1f8367758c0151a
Alberto Blanco <albertoblanco.s@gmail.com>
2014-09-29 14:22:01 -0700

    Add log in / log out and remember me options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,14 +15,36 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
   	else
   		render 'new'
   	end
   end
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
   private
   	def user_params
   		params.require(:user).permit(:name,:email,:password,:password_confirmation)
   	end
+    # Returns true if the given token matches the digest.
+    def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
